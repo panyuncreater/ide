60906fdf5b06d08767313ac8b01a5314c41d28ea
panyuncreater <1265110878@qq.com>
2026-07-01 22:21:39 +0800

    无关文件清除，重新配置CMake

--- out/build/debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/v/Desktop/ide/ide
-# Build directory: C:/Users/v/Desktop/ide/ide/out/build/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- test_harness/build2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call "D:\vs\Common7\Tools\VsDevCmd.bat" -arch=amd64 >nul 2>&1
-set PROJ=C:\Users\v\Desktop\ide\ide
-set OUT=%PROJ%\test_harness
-cl /nologo /EHsc /std:c++17 /I"%OUT%" /I"%PROJ%" /Fo"%OUT%\\" "%OUT%\test_main.cpp" "%PROJ%\lexer\Lexer.cpp" "%PROJ%\parser\Parser.cpp" "%PROJ%\ast\ASTNode.cpp" "%PROJ%\interpreter\Interpreter.cpp" "%PROJ%\interpreter\Environment.cpp" /Fe"%OUT%\test_runner.exe" > "%OUT%\build_log.txt" 2>&1
-echo EXIT_CODE=%ERRORLEVEL% >> "%OUT%\build_log.txt"
